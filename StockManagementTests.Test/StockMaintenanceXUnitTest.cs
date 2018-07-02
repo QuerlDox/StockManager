ca2ce47389330bccb96c045b900cc493c9e0c131
HaroldFinch <incognitoshadow101@gmail.com>
2018-07-02 20:30:39 +0800

    Unit Tested StockMaintenance class

--- StockManagementTests.Test/StockMaintenanceXUnitTest.cs
+++ StockManagementTests.Test/StockMaintenanceXUnitTest.cs
@@ -17,26 +17,18 @@ namespace StockManagementXUnitTests.Test
 
         private StockSystem.StockManagement.StockMaintenance stockMaintenanceObj = new StockSystem.StockManagement.StockMaintenance(stockInfo);
 
+        private Product beefPatty;
+        private Product buns;
+
+        private StockSystem.StockManagement.Stock stock1;
+        private StockSystem.StockManagement.Stock stock2;
 
 
 
         [Fact]
         public void AddProductToStock_shouldAddProductToStock() {
-            Product beefPatty = new Product
-            {
-                ProductCode = "1",
-                ProductName = "beefPatty",
-                ProductStatus = 0
-            };
 
-
-            Product buns = new Product
-            {
-                ProductCode = "2",
-                ProductName = "Buns",
-                ProductStatus = 0
-            };
-
+            initializeStockFile();
 
             // additional Product
             Product lettuce = new Product
@@ -46,17 +38,7 @@ namespace StockManagementXUnitTests.Test
                 ProductStatus = 0
             };
 
-            StockSystem.StockManagement.Stock stock1 = new StockSystem.StockManagement.Stock()
-            {
-                Product = beefPatty,
-                Quantity = 5
-            };
-
-            StockSystem.StockManagement.Stock stock2 = new StockSystem.StockManagement.Stock()
-            {
-                Product = buns,
-                Quantity = 5
-            };
+  
 
             // additional stock
             StockSystem.StockManagement.Stock stock3 = new StockSystem.StockManagement.Stock() {
@@ -90,28 +72,16 @@ namespace StockManagementXUnitTests.Test
         [Fact]
         public void UpdateProductQty_shouldChangeProductQtyInStock_inXunit()
         {
-            Product beefPatty = new Product
-            {
-                ProductCode = "1",
-                ProductName = "beefPatty",
-                ProductStatus = 0
-            };
-
-            Product buns = new Product
-            {
-                ProductCode = "2",
-                ProductName = "Buns",
-                ProductStatus = 0
-            };
 
 
+            initializeStockFile();
 
             int expected = 5;
           
                 stockMaintenanceObj.AddProductToStock(beefPatty, 20);
                 stockMaintenanceObj.AddProductToStock(buns, 100);
                 stockMaintenanceObj.UpdateProductQty(beefPatty, 5);
-                stockMaintenanceObj.UpdateProductQty(buns, 5);
+                stockMaintenanceObj.UpdateProductQty(buns, 3);
          
 
             int actual = stockMaintenanceObj.GetStockQty(beefPatty);
@@ -122,20 +92,7 @@ namespace StockManagementXUnitTests.Test
         public void GetStockOnHand_shouldReturnStockOnHand_inXUnit()
         {
 
-            Product beefPatty = new Product
-            {
-                ProductCode = "1",
-                ProductName = "beefPatty",
-                ProductStatus = 0
-            };
-
-
-            Product buns = new Product
-            {
-                ProductCode = "2",
-                ProductName = "Buns",
-                ProductStatus = 0
-            };
+            initializeStockFile();
 
             // additional Product
             Product lettuce = new Product
@@ -145,17 +102,7 @@ namespace StockManagementXUnitTests.Test
                 ProductStatus = 0
             };
 
-            StockSystem.StockManagement.Stock stock1 = new StockSystem.StockManagement.Stock()
-            {
-                Product = beefPatty,
-                Quantity = 5,
-            };
 
-            StockSystem.StockManagement.Stock stock2 = new StockSystem.StockManagement.Stock()
-            {
-                Product = buns,
-                Quantity = 5,
-            };
 
 
             // added stock
@@ -188,6 +135,90 @@ namespace StockManagementXUnitTests.Test
         }
 
 
-     }
+        internal void initializeStockFile() {
+
+             beefPatty = new Product
+            {
+                ProductCode = "1",
+                ProductName = "beefPatty",
+                ProductStatus = 0
+            };
+
+
+             buns = new Product
+            {
+                ProductCode = "2",
+                ProductName = "Buns",
+                ProductStatus = 0
+            };
+
+
+                stock1 = new StockSystem.StockManagement.Stock()
+            {
+                Product = beefPatty,
+                Quantity = 5,
+            };
+
+                stock2 = new StockSystem.StockManagement.Stock()
+            {
+                Product = buns,
+                Quantity = 3,
+            };
+
+        }
+
+        [Fact] 
+        public void GetStockQuantity_shouldReturnQtyOfProductInStock() {
+           
+            // Arrange
+            initializeStockFile();
+
+            int expected = 3;
+
+            // Assert
+            int actual = stockMaintenanceObj.GetStockQty(buns);
+
+            Assert.Equal(expected, actual);
+         
+        }
+
+        [Fact]
+        public void RemoveProductFromStock_shouldRemovetTheProductFromTheStock() {
+            // Arrange
+            initializeStockFile();
+
+            List<StockSystem.StockManagement.Stock> StockMockList = new List<StockSystem.StockManagement.Stock>();
+            {
+                StockMockList.Add(stock1);
+                StockMockList.Add(stock2);
+               
+            }
+
+
+            // Arrange 
+            List<StockSystem.StockManagement.Stock> expected = StockMockList;
+
+
+            // additional Product
+            Product lettuce = new Product
+            {
+                ProductCode = "3",
+                ProductName = "Lettuce",
+                ProductStatus = 0
+            };
+
+
+
+
+            // Action
+            stockMaintenanceObj.RemoveProductFromStock(lettuce);
+            List<Stock> actual = stockMaintenanceObj.GetStocksOnHand();
+
+            expected.Should().BeEquivalentTo(actual);
+
+        }
+
+
+    }
 
 }
